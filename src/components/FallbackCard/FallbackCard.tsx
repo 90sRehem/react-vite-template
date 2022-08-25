2f632a06cfc3329db6d34892b7c43b18beba0b51
jonathan <jonathan.de.oliveira@live.com>
2022-08-25 16:02:37 -0300

    chore: fixate error icon

--- src/components/FallbackCard/FallbackCard.tsx
+++ src/components/FallbackCard/FallbackCard.tsx
@@ -8,25 +8,46 @@ import {
   CloseIcon,
   InfoIcon,
   WarningTwoIcon,
+  Button,
 } from "@/lib/chakra-ui";
 
 export interface IFallBackCardProps {
   type: "success" | "error" | "info" | "warning";
   headingText: string;
   descriptionText: string;
+  fallbackBtn?: boolean;
+}
+
+function ErrorIcon() {
+  return (
+    <Box display="inline-block">
+      <Flex
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        bg={'red.500'}
+        rounded={'50px'}
+        w={'55px'}
+        h={'55px'}
+        textAlign="center">
+        <CloseIcon boxSize={'20px'} color={'white'} />
+      </Flex>
+    </Box>
+  )
 }
 
 const icons = {
   success: <CheckCircleIcon boxSize="50px" color="green.500" />,
-  error: <InfoIcon boxSize="50px" color="blue.500" />,
-  info: <CloseIcon boxSize="20px" color="white" />,
+  info: <InfoIcon boxSize="50px" color="blue.500" />,
+  error: <ErrorIcon />,
   warning: <WarningTwoIcon boxSize="50px" color="orange.300" />,
 };
 
 export function FallbackCard({
   descriptionText,
   headingText,
-  type,
+  type = "info",
+  fallbackBtn = false
 }: IFallBackCardProps) {
   return (
     <Flex alignItems="center" justifyContent="center">
@@ -43,6 +64,9 @@ export function FallbackCard({
           {headingText}
         </Heading>
         <Text color="gray.500">{descriptionText}</Text>
+        {fallbackBtn && <Button onClick={() => window.location.assign(window.location.origin)}>
+          Refresh
+        </Button>}
       </Box>
     </Flex>
   );
