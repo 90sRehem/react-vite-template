36c749f186dd6870fa3a9952316f76b17fe5de4f
90sRehem <you@example.com>
2022-08-25 14:06:54 -0300

    feat: add goback button and pagetitle

--- src/components/Layout/ContentLayout.tsx
+++ src/components/Layout/ContentLayout.tsx
@@ -1,12 +1,26 @@
-import { Stack, useColorModeValue } from "@/lib/chakra-ui";
+import {
+  ArrowBackIcon,
+  Flex,
+  Heading,
+  IconButton,
+  Stack,
+  Tooltip,
+  useColorModeValue,
+} from "@/lib/chakra-ui";
 import { ReactNode } from "react";
 import { useSidebarSizeStore } from "../Sidebar/stores/sidebarSizeStore";
 
 interface ContentLayoutProps {
   children: ReactNode;
+  goBackButton?: boolean;
+  pageTitle?: string;
 }
 
-export function ContentLayout({ children }: ContentLayoutProps) {
+export function ContentLayout({
+  children,
+  goBackButton,
+  pageTitle,
+}: ContentLayoutProps) {
   const { navSize } = useSidebarSizeStore();
 
   return (
@@ -19,7 +33,22 @@ export function ContentLayout({ children }: ContentLayoutProps) {
       minH="100vh"
       minW="100vw"
     >
-      {children}
+      <>
+        <Flex p="2">
+          {goBackButton && (
+            <Tooltip hasArrow>
+              <IconButton
+                variant="outline"
+                aria-label="go back"
+                icon={<ArrowBackIcon />}
+                size="lg"
+              />
+            </Tooltip>
+          )}
+          {pageTitle && <Heading ml="2">{pageTitle}</Heading>}
+        </Flex>
+        {children}
+      </>
     </Stack>
   );
 }
