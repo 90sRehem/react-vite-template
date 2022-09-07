e5f9a7c3a5f045c9f28362ae0f98726ce5908480
90sRehem <you@example.com>
2022-09-07 19:52:58 -0300

    feat: add button + loading indicator; styles: fix darkmode colors

--- src/features/users/components/UsersList.tsx
+++ src/features/users/components/UsersList.tsx
@@ -1,5 +1,11 @@
-import { Flex, Heading, Spinner, Box } from "@/lib/chakra-ui";
-import { IUser } from "@/types";
+import {
+  Flex,
+  Heading,
+  Spinner,
+  Box,
+  Button,
+  useColorModeValue,
+} from "@/lib/chakra-ui";
 import { useState } from "react";
 import { useUsers } from "../api";
 import { UsersTable } from "./UsersTable";
@@ -15,14 +21,20 @@ export function UsersList() {
   return (
     <Box>
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-        <Box flex="1" borderRadius={8} bg="pGray.800" p="8">
+        <Box
+          flex="1"
+          borderRadius={8}
+          bg={useColorModeValue("gray.200", "gray.800")}
+          p="8"
+        >
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
-              {isLoading && isFetching && (
+              {!isLoading && isFetching && (
                 <Spinner size="sm" color="500.gray" ml="4" />
               )}
             </Heading>
+            <Button>Criar novo usuário</Button>
           </Flex>
           <UsersTable
             data={data?.users || []}
